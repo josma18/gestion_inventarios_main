e4e897e0a58c525b3219a97c516a16bc1b74c70e
Jose Manuel Diaz Castañeda <josemanueldiazc20@gmail.com>
2024-12-20 10:36:49 -0500

    update: archivos

---
+++ src/config/db.config.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_TYPE, DB_USERNAME } from "../shared/constants";
+import { Proveedor } from "../entities/proveedor";
+import { Categoria } from "../entities/categoria"; // Asegúrate de que la ruta sea correcta
+
+export const AppDataSource = new DataSource({
+    type: DB_TYPE as any,
+    host: DB_HOST,
+    port: Number(DB_PORT||'0'),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    entities: [Proveedor, Categoria],
+});
