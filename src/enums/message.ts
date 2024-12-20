8ce350c27ed0c5d0401f9f9d93eafe3f7ee7a209
Jose Manuel Diaz Castañeda <josemanueldiazc20@gmail.com>
2024-12-20 11:09:46 -0500

    add: nuevaActualizacion

--- src/enums/message.ts
+++ src/enums/message.ts
@@ -1,5 +1,8 @@
-export enum message {
+export enum Message {
     INSERTADO_OK = 'Insertado Correctamente',
     ACTUALIZADO_OK = 'Actualizado Correctamente',
-    ELIMINADO_OK = 'Eliminado Correctamente'
+    ELIMINADO_OK = 'Eliminado Correctamente',
+
+    NOT_FOUND = 'Recurso no ha sido encontrado'
+
 }
---
+++ src/services/proveedor.service.ts
@@ -0,0 +1,41 @@
+import { AppDataSource } from "../config/db.config";
+import { Proveedor } from "../entities/proveedor";
+import { EstadoAuditoria } from "../enums/estado-auditoria";
+
+const repository = AppDataSource.getRepository(Proveedor);
+
+export const insertarProveedor = async (data: Partial<Proveedor>): Promise<Proveedor> => {
+    const newProveedor: Proveedor = await repository.save(data);
+    return await repository.findOne({where: { idProveedor: newProveedor.idProveedor }});
+}
+
+export const listarProveedor = async (): Promise<Proveedor[]> => {
+    return await repository.find({where: { estadoAuditoria: EstadoAuditoria.ACTIVO }});
+}
+
+export const obtenerProveedor = async (idProveedor: number): Promise<Proveedor> => {
+    return await repository.findOne({where: { idProveedor, estadoAuditoria: EstadoAuditoria.ACTIVO }});
+}
+
+export const actualizarProveedor = async (idProveedor: number, data: Partial<Proveedor>): Promise<Proveedor> => {
+   await repository.update(idProveedor,data);
+   return obtenerProveedor(idProveedor);
+}
+
+export const darBajaProveedor = async (idProveedor: number): Promise<void> => {
+    await repository.update(idProveedor,{estadoAuditoria: EstadoAuditoria.INACTIVO});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
